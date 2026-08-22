7facd11284b869fe7dc9e655412472d6fc48c701
jaxksonm <jacksonmcdonald9@gmail.com>
2026-08-22 11:48:04 -0400

    finished product

--- testing.py
+++ testing.py
@@ -1,2 +1,25 @@
-for i in range(10): 
-    print("hello world")
+import random 
+
+
+def get_movie():
+    return input("What movie do you want to watch? \n")
+
+def generate_phone_number():
+    phone_number = random.randint(10**9, (10**10) - 1)
+    return phone_number
+
+def match_maker(movie, number):
+    number_str = str(number)
+    formatted = "(" + number_str[:3] + ")" + number_str[3:]
+    print(f"Movie: {movie}")
+    print(f"Phone Number: {formatted}")
+
+
+def main():
+    movie = get_movie()
+    number = generate_phone_number()
+    match_maker(movie, number)
+
+
+if __name__ == "__main__":
+    main()
