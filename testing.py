ce7848572047e3eca73a7fc7a479a638ef06e5e7
jaxksonm <jacksonmcdonald9@gmail.com>
2026-08-22 11:58:36 -0400

    final product part 2

--- testing.py
+++ testing.py
@@ -1,6 +1,14 @@
 import random 
 
 
+names = ["Olivia", "Sophia", "Isabella", "Ava", "Mia", "Charlotte", 
+               "Amelia", "Harper", "Evelyn", "Luna", "Aria", "Ella", 
+               "Scarlett", "Grace", "Chloe", "Victoria", "Riley", 
+               "Aubrey", "Zoey", "Layla"]
+
+def get_name():
+    return random.choice(names)
+
 def get_movie():
     return input("What movie do you want to watch? \n")
 
@@ -8,18 +16,21 @@ def generate_phone_number():
     phone_number = random.randint(10**9, (10**10) - 1)
     return phone_number
 
-def match_maker(movie, number):
+def match_maker(movie, number, name):
     number_str = str(number)
     formatted = "(" + number_str[:3] + ")" + number_str[3:]
     print(f"Movie: {movie}")
+    print(f"Name: {name}")
     print(f"Phone Number: {formatted}")
+    
 
 
 def main():
     movie = get_movie()
+    name = get_name()
     number = generate_phone_number()
-    match_maker(movie, number)
+    match_maker(movie, number, name)
 
 
 if __name__ == "__main__":
-    main()
+    main()  
